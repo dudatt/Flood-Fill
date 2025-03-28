48c82fde01d95d365603926c3da9e9873ad0071c
julia-tatim <116753015+julia-tatim@users.noreply.github.com>
2025-03-28 16:31:43 -0300

    Inicio do FloodFill
    Display da imagem desejada e identificação da cor do pixel inicial

--- src/FloodFill.java
+++ src/FloodFill.java
@@ -1,3 +1,30 @@
-public class FloodFill {
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+public class FloodFill {
+    private BufferedImage image;
+    private int width;
+    private int height;
+    private ImgLoader imgLoader;
+
+    public FloodFill(String caminho) throws IOException  {
+        image = ImageIO.read(new File(caminho));
+        width = image.getWidth();
+        height = image.getHeight();
+        this.imgLoader = new ImgLoader();
+        imgLoader.display(image);
+    }
+
+    public void floodFillStack(int xInicial, int yInical, int corNova){
+        int corInicial = image.getRGB(xInicial, yInical);
+        Stack<Object> stack = new Stack<>();
+        stack.push(new int[]{xInicial, yInical});
+        System.out.println(corInicial);
+    }
+
+    public void teste(){
+        imgLoader.display(image);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import javax.swing.*;
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        try {
+            FloodFill stack = new FloodFill("src/base.jpg");
+            stack.floodFillStack(100, 100, 0);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
