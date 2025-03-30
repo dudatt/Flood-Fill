9d76c3fc4091b4c36b8ac55d490b9f25a8367ea5
dudatt <dudamelobarbosa@gmail.com>
2025-03-30 12:16:40 -0300

    feat: instanciando queue

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
+=======
+        try {
+
+            // Imagem com Stack
+            FloodFill stackFloadFill = new FloodFill("src/base.png");
+            stackFloadFill.floodFillQueue(100, 100, -65536);
+            stackFloadFill.salvarImagem("src/output/stack-output.png");
+
+            //Imagem com Queue
+            //FloodFill queueFloaFill = new FloodFill("src/base.png");
+            //queueFloaFill.floodFillQueue(100, 100, -65536);
+            //queueFloaFill.salvarImagem("src/output/queue-output.png");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+>>>>>>> Stashed changes
     }
 }
